1540050faae625e096475da83a1a71643cf9c2db
ingfranja082022 <ing.franja1050@gmail.com>
2026-08-20 09:46:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 
 // 1. Permitir CORS para desarrollo y producción
 app.use(cors({
